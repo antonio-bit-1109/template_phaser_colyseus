2ea9ebefd2e954583166aade6385d51ee360df82
Antonio Rizzuti <antonio.rizzuti@nttdata.com>
2025-12-31 14:15:03 +0100

    Revert "Refactored bonus handling logic to simplify collision detection and bonus scaling mechanics. Removed unused methods, streamlined `UtilsClient` and `UtilsServer` utilities, and enhanced `PongRoom.ts` with clear bonus reset and cooldown management. Improved scene memory management by removing outdated bonus-related event logic."
    This reverts commit 0c076931188f9643f6c59ca8b61238746a7cd7a4.

--- src/client/game/util/UtilsClient.ts
+++ src/client/game/util/UtilsClient.ts
@@ -5,12 +5,56 @@ export class UtilsClient {
 
     private readonly cursors;
 
+    // private timeout;
+
     constructor(scene?: Scene) {
         if (scene) {
             this.cursors = scene?.input?.keyboard?.createCursorKeys();
 
         }
     }
+
+    // public checkBonusAndModifySprite(bonus: BonusSchema, player: Sprite, room: Colyseus.Room, playerSchema: PlayerSchema) {
+    //
+    //     // se bonus è un growup aumento dimensioni dello sprite e aumento raggio del playerschema
+    //     // setscale = 0.3 --- r= 30;
+    //     // setscale = 0.5 --- r = 45;
+    //     if (bonus.type.toLowerCase() === "growup") {
+    //
+    //         // controllo se l ultimo hitbox del bonus è a destra o sinistra dello schermo,
+    //         // questo mi dice quale player è stato colpito
+    //         // e quindi a quale player applicare il bonus
+    //         if (bonus.hitbox_x < 500 && playerSchema.index === 1) {
+    //             player.setScale(0.5)
+    //             const message: IMessage = {
+    //                 player_r: 45
+    //             }
+    //             room.send("change_r_player", message)
+    //         }
+    //
+    //         if (bonus.hitbox_x > 500 && playerSchema.index === 2) {
+    //             player.setScale(0.5)
+    //             const message: IMessage = {
+    //                 player_r: 45
+    //             }
+    //             room.send("change_r_player", message)
+    //         }
+    //
+    //         // notifica al server per far tornare il player alla dimensione originale
+    //
+    //         // timeout per far tornare il player a dimensione originale
+    //         // this.timeout = setTimeout(() => {
+    //         //     let scale = player.scale
+    //         //     if (scale > 0.3) {
+    //         //         // player.setScale(0.3)
+    //         //         const message: IMessage = {
+    //         //             player_r: 30
+    //         //         }
+    //         //         room.send("change_r_player", message)
+    //         //     }
+    //         // }, 5000)
+    //     }
+    // }
 
     public returnBasicConfigurationStyle(
         color?: string,
